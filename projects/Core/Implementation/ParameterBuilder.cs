99befc86ad1fabac08d40c91096637caf720ad87
rickyah <rickyah@380802de-8548-11dd-99d6-6900107db72b>
2009-01-18 10:36:03 +0000

    *Added documentation comments to this class
    *Changed class operations to match changes in IParameterBuilder

--- projects/Core/Implementation/ParameterBuilder.cs
+++ projects/Core/Implementation/ParameterBuilder.cs
@@ -4,12 +4,35 @@ using System.Text;
 
 namespace LateBindingHelper.Implementation
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ParameterBuilder : IParameterBuilder
     {
+        #region Private Members
+        /// <summary>
+        /// Holds the list of parameters
+        /// </summary>
         private List<object> _parameters = new List<object>();
-        private List<bool> _isRef = new List<bool>();
+
+        /// <summary>
+        /// Holds which position in the list of parameters
+        /// is passed with by reference semantics of by value
+        /// semantics
+        /// </summary>
+        private List<bool> _isRef = new List<bool>(); 
+        #endregion
+
         #region Miembros de IParameterList
 
+        /// <summary>
+        /// Adds a parameter to the list which is meant to be called using by value semantics.
+        /// </summary>
+        /// <param name="value">An object with the parameter value.</param>
+        /// <returns>
+        /// A reference to the <see cref="IParameterBuilder"/>instance which
+        /// called this method.
+        /// </returns>
         public IParameterBuilder AddParameter(object value)
         {
             _parameters.Add(value);
@@ -18,6 +41,14 @@ namespace LateBindingHelper.Implementation
             return this;
         }
 
+        /// <summary>
+        /// Adds a parameter to the list which is meant to be called using by reference semantics.
+        /// </summary>
+        /// <param name="value">An object with the parameter value.</param>
+        /// <returns>
+        /// A reference to the <see cref="IParameterBuilder"/>instance which
+        /// called this method.
+        /// </returns>
         public IParameterBuilder AddRefParameter(object value)
         {
 
@@ -27,27 +58,29 @@ namespace LateBindingHelper.Implementation
             return this;
         }
 
+        /// <summary>
+        /// Removes all parameters in the list
+        /// </summary>
         public void Clear()
         {
             _parameters.Clear();
             _isRef.Clear();
         }
-        public object[] AsArray
-        {
-            get
-            { 
-                return _parameters.ToArray(); 
-            }
 
-            set
-            {
-                for (int i = 0; i <value.Length; ++i)
-                {
-                    _parameters[i] = value[i];
-                }
-            }
+        /// <summary>
+        /// Access the parameter list as an array
+        /// </summary>
+        /// <returns></returns>
+        public object[] GetParametersAsArray()
+        {
+            return _parameters.ToArray(); 
         }
 
+        /// <summary>
+        /// Indexer to the parameter list
+        /// </summary>
+        /// <value></value>
+        /// <returns>The parameter </returns>
         public object this[int index]
         {
             get
@@ -56,11 +89,23 @@ namespace LateBindingHelper.Implementation
             }
         }
 
-        public IList<bool> ISReferenceParameter
+        /// <summary>
+        /// Gets a list of booleans with the same length as the total
+        /// number of parameters in the list. Each position in the
+        /// array of booleans has the value <c>true</c> or <c>false</c>
+        /// if the parameter with the same position index
+        /// is passed by reference or not respectively.
+        /// </summary>
+        /// <returns>An IList of booleans.</returns>
+        public IList<bool> GetReferenceParameterList()
         {
-            get { return _isRef.AsReadOnly();  }
+            return _isRef.AsReadOnly();
         }
 
+        /// <summary>
+        /// Returns the total number of parameters in the list.
+        /// </summary>
+        /// <value></value>
         public int Count
         {
             get { return _parameters.Count; }
