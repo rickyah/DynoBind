76aad22cd0447b0775eaddc1e7dfad7b2ba28fba
rickyah <rickyah@380802de-8548-11dd-99d6-6900107db72b>
2009-02-03 23:07:49 +0000

    REFACTORING: Separate contracts

---
+++ projects/Core/IMethodLastCallParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LateBindingHelper
+{
+    /// <summary>
+    /// Represents the parameters used in a Method invocation
+    /// </summary>
+    public interface IOperationLastCallParameters
+    {
+        /// <summary>
+        /// Returns the value of the parameters after the call to any
+        /// <see cref="IMethodInvoker.Invoke"/> method
+        /// Only usefull to retrieve the values of the call that were passed as
+        /// reference, and thus has been potentially modified.
+        /// </summary>
+        /// <remarks>
+        /// All parameters passed to the method are referenced after the call to an
+        /// <see cref="IMethodInvoker.Invoke"/> method, either they were passed as
+        /// reference or not.
+        /// </remarks>
+        object[] LastCallParameters { get;}
+    }
+}
